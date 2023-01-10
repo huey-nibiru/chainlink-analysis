4e7a80191d95ea20c76f55244d9e26d072d8d94b
Yousef Macer <yousefmacer@gmail.com>
2023-01-10 16:33:46 -0500

    Update ecosystem.py

--- chainlink/ecosystem.py
+++ chainlink/ecosystem.py
@@ -1,6 +1,8 @@
 from imports import *
 
-# Three files are included, the project category, chains involved, and the chainlink products used
+
+
+# Three files are included, the integration/project category, chains involved, and the chainlink products used
 # Each product is able to be sorted by each of these parameters
 f1 = MAIN_PATH + "categories.txt"
 f2 = MAIN_PATH + "chains.txt"
@@ -16,10 +18,10 @@ for i in [f1,f2,f3]:
         if i==f3:products=d
 
 
+
 # Opens the window using the webdriver, and finds each element in the body by its tag name
 # Returns list 'e' of the current chainlink ecosystem 
 # Each element of the list contains the link of one integration/project utilizing chainlink protocols
-
 def scrape_links(url):
     e = []
     driver = webdriver.Chrome(DRIVER_PATH)
@@ -45,11 +47,12 @@ def scrape_links(url):
 
 
 
-
-# Create a struct for the project 
+# Creates a object for each integration/project 
+# Returns a Beautiful Soup object for each div element containing an integration/project
 # Each project has the following fields: name, status, chains, category, integrations, year
+# return the object 'integration' 
 def parse_data(partner):
-    partner_struct = {
+    integration = {
             "name":None,
             "status":None,
             "chains":[],
@@ -60,26 +63,28 @@ def parse_data(partner):
     req = requests.get(partner)
     soup = BeautifulSoup(req.content, "html.parser")
     data = soup.findAll("div", {"class": "product-profile-text-image-wrapper"})
-    partner_struct["name"] = soup.find("div", {"class": "product-profile-name"}).text.lower()
-    partner_struct["status"] = soup.find("div", {"class": "live-text"}).text.lower()
+    integration["name"] = soup.find("div", {"class": "product-profile-name"}).text.lower()
+    integration["status"] = soup.find("div", {"class": "live-text"}).text.lower()
     
     for elem in data:
         elem = elem.text.lower()
         if elem in categories:
-            partner_struct["category"] = elem
+            integration["category"] = elem
         elif elem in chains:
-            partner_struct["chains"].append(elem)
-            partner_struct["chains"] = [*set(partner_struct["chains"])]
+            integration["chains"].append(elem)
+            integration["chains"] = [*set(integration["chains"])]
         elif elem in products:
-            partner_struct["integrations"].append(elem)
-            partner_struct["integrations"] = [*set(partner_struct["integrations"])]
+            integration["integrations"].append(elem)
+            integration["integrations"] = [*set(integration["integrations"])]
         elif elem in ["2017","2018","2019","2020","2021","2022"]:
-            partner_struct["year"] = elem        
-    return  partner_struct
+            integration["year"] = elem        
+    return  integration
 
 
 
-
+# Links the previous functions together to scrape the chainlink ecosystems url 
+# Includes a loading bar to track progress (progres.bar)
+# Adds the integration to the json file 'link_partnerships.json'
 def scrape_ecosystem():
     system("clear")
     from progress.bar import Bar
