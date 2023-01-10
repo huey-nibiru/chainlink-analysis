03f51653103c5db6a27cf5ea986d7fad5cd1260c
Yousef Macer <yousefmacer@gmail.com>
2023-01-10 18:20:49 -0500

    Update imports.py

--- chainlink/imports.py
+++ chainlink/imports.py
@@ -9,10 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
- # Opening JSON file
-with open('/Users/Yousefmacer/Github/data_analytics/chainlink_analysis/chainlink/config.json') as f:
+with open('/Users/Yousefmacer/Github/data/chainlink_analysis/chainlink/config.json') as f:
     data = json.load(f)
 
 MAIN_PATH = data["path"]
